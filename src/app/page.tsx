957ef6fb3438b436dfa6ac5f2b1b48747daad70b
chrit <chrit@pop-os.localdomain>
2023-11-14 22:06:11 +0100

    first draft navbar, tried next Link object

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,13 @@
 import Image from 'next/image'
 import styles from './page.module.css'
 import pizzas from "./pizzadata.json"
+import NavBar from './components/Header'
+import "./components/navbar.css" 
 
 export default function Home() {
   return (
     <main className={styles.main}>
+      <NavBar/>
       <h1>VELKOMMEN TIL</h1>
       <h1>PIZZA-O-METER</h1>
       {pizzas.map(pizza => <p>{pizza.name} ------- {pizza.price}</p>)}
