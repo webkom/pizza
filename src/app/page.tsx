edc0b98c47a60db707690c304fd369ab42254032
Magnus Brecke <magnbrec@stud.ntnu.no>
2023-11-21 20:34:53 +0100

    Connecting page with modal component

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,6 +3,11 @@ import "./Page.css"
 import pizzas from "./pizzadata.json"
 import NavBar from './components/Header'
 import "./components/navbar.css" 
+import Modal from "./components/Modal"
+
+const ModalDisplay = (id: number) => {
+  document.getElementsByClassName("ModalHent")[0].innerHTML="<Modal id_nummer="+id.toString()+"/>"
+}
 
 export default function Home() {
   return (
@@ -13,11 +18,13 @@ export default function Home() {
       <h1 className='Header'>VELKOMMEN TIL PIZZA-O-METER</h1>
       
       
-     <div className='PizzaBox'>{pizzas.map(pizza => <button><div><img src={pizza.img} className='PizzaImg'></img>
+     <div className='PizzaBox'>{pizzas.map(pizza => <button onClick={ModalDisplay(id)}><div><img src={pizza.img} className='PizzaImg'></img>
                                                          <p className='PizzaName'>{pizza.name}</p>
                                                          <p className='PizzaPrice'>{pizza.price},- kr</p> 
                                                          <p className='Stars'>&#9734;&#9734;&#9734;&#9734;&#9734;</p></div></button>)}</div>
 
+
+    <div className="ModalHent"></div>
     </main>
   )
 }
